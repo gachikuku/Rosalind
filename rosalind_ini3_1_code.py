54e5dc73bfd7613fcc54101bbee73ce5511edeca
Alnoman Kamil <alneihzen@gmail.com>
2021-10-23 22:39:42 +0300

    Create rosalind_ini3_1_code.py

---
+++ rosalind_ini3_1_code.py
@@ -0,0 +1,8 @@
+s = "ssy74VEdUHWEEUaahQSkPdlc0uFwFmrPIanHQVifoBfuPgbDPQrZH6Sp4GKbiS48XmGS2fUdcwm0fBrantajQxKqYpkdxRZI5ZdceZGBGzooQkJtcpr4Wmnmg7Ha0wvWoMuvDxtW3F0dYj66Yrusticolus6y3mvwgx3vrrJ1"
+
+def slice(s, a, b, c, d):
+
+    print(s[a:b+1],s[c:d+1])
+    
+
+slice(s, 77, 82, 145, 154)
