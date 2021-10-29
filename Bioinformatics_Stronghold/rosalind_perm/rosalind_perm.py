b96ddc0b262b975709db242113d10b67dc921105
Alnoman Kamil <alneihzen@gmail.com>
2021-10-29 23:58:17 +0300

    Create rosalind_perm.py

---
+++ Bioinformatics_Stronghold/rosalind_perm/rosalind_perm.py
@@ -0,0 +1,38 @@
+def fact(n: int) -> int:
+
+    ans = 1
+
+    for i in range(1,n+1):
+    
+        ans *= i
+        
+    return ans
+        
+        
+
+def permute(n: int) -> list:
+
+    result_perms = [[]]
+  
+    for n in range(1,n+1):
+  
+        new_perms = []
+    
+        for perm in result_perms:
+    
+            for i in range(len(perm)+1):
+      
+                new_perms.append(perm[:i]+[n]
+                                +perm[i:])
+        
+                result_perms = new_perms
+        
+    return result_perms
+
+
+
+print(fact(7))
+
+for i in permute(7):
+
+    print(*i)
